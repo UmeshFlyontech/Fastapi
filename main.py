1d893e99afd6722984a8da7d7fd9af5d776de780
SudRK <104437208+SudRK@users.noreply.github.com>
2022-06-22 14:51:53 +0530

    Updated main

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
+
 
 app = FastAPI()
 
-@app.get("/")
-def index():
-    return {"Data":{"Name":"Sudip"}}
+@app.get("/blog")
+def index(limit: int=20, published: bool=True, sort: Optional[str]= None):
+    if published:
+        return {'Data': f'{limit} published blogs from the DB'}
+    else:
+        return {'Data': f'{limit} blogs from the DB'}
 
 @app.get("/blog/unpublished")
 def  unpublished():
@@ -15,5 +21,16 @@ def blog(id: int):
     return {"Data":id}
 
 @app.get("/blog/{id}/comment")
-def comment(id):
+def comment(id, limit=10):
+    return limit
     return {"Data":{'1', '2'}}
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+    
+@app.post("/blog")
+def create_blog(request: Blog):
+    return request
+    return {"Data": "Blog is created"}
